601ff7c8075ea4c73fc4144917a54fd0a6be935d
sokret sathit <sokretsathit13@gmail.com>
2022-08-12 22:44:19 -0500

    Added enemy droppng loots and hero picking up and stroes it

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// 1. Console must greet player with a fun message
+// 2. Console must ask for the player's name and store it
+// 3. Walking:
+// - The console will ask the user to enter a "w" to walk
+// - Every time the player walks, a random algorithm will be run that determines if a wild enemy has appeared (A 1/3 or 1/4 chance of being attacked)
+// - Use a while loop to control this flow.
+// 1. If a wild enemy appears:
+// - The enemy is random (can be chosen out of a minimum of 3 different enemy names)
+// - The user can decide to attack or run
+// - If attacking, a random amount of damage will be dealt between a min and max
+// - If running, there will be a 50% chance of escaping
+// - After the player attacks or runs the enemy attacks back for a random damage amount
+// - The player and enemy will attack each other in a loop until one of them passes out or dies.
+// - If the player kills the enemy you can give the Player some HP and a special item that is stored in the inventory. After this, the player will continue walking.
+// - If the enemy kills the player the console prints a cool death message and the game ends
+// 1. Inventory
+// - When the player kills enemies, they are awarded with items
+// - If the user enters 'ventory
 const readlineSync = require('readline-sync');
 console.log("Ahh you're finally awake! What is your name?");
 const name = readlineSync.question("You! tell me your name at least, before we get our heads chopped off. \n Tell him your name >>> ");
@@ -94,8 +112,8 @@ function game() {
 
                 const user = readlineSync.question('Enter "r" to run, or enter "a" to attack! ');
 
-                // let inGame = true;
-                // let inBattle = false;
+                let inGame = true;
+                let inBattle = false;
 
 
                 switch (user) {
@@ -115,13 +133,15 @@ function game() {
                         console.log('Enemy attacked you with ' + enemyCriticalHit + ' Hit Points.');
                         
                         let loot = 'boots'
-
-                        if (enemyHealth <= 0) {
-                            console.log('Nice work! You defeated ' + enemy + '.\n' + enemy + ' left behind: ' + loot ); // + pickUp
-                            let loot = Math.random();
-                            if (loot === 'boots') {
-                                pickUp(loot);
-                            }
+                        if (enemy = dead ){
+                            inventory.push(loot) 
+                       
+                        // if (enemyHealth <= 0) {
+                        //     console.log('Nice work! You defeated ' + enemy + '.\n' + enemy + ' left behind: ' + loot ); // + pickUp
+                        //     let loot = Math.random();
+                        //     if (loot === 'boots') {
+                        //         pickUp(loot);
+                        //     }
                         } else if (userHealth <= 0) {
                             console.log(enemy + ' has defeated you. You died ☠️! You will respawn at the nearest keep.');
                         }
@@ -130,7 +150,7 @@ function game() {
         }
     }
 }
-
+function 
 while(player.userHealth > 0) {
     userRestore = function() {
         userActive = true;
--- game.js
+++ game.js
@@ -1,16 +1,18 @@
 const readlineSync = require('readline-sync');
 const name = readlineSync.question("Ahh you're finally awake! What is your name?");
+// const inventory = readlineSync
 class Player {
     constructor(name, playerHealth, inventory) {
         this.playerName = name,
         this.playerHealth = playerHealth,
         this.inventory = []
+        //Why player doesn't start with a weapon??
     }
     isAlive(){
         return this.playerHealth > 0
     }
 }
-const hero = new Player ('hero', 20)
+let hero = ['hero', 20, "Trouser"]
 class enemies {
     constructor(name, enemyHealth, loots) {
         this.enemyName = name,
@@ -20,7 +22,14 @@ class enemies {
 }
 let helgenGuard = new enemies ("Helgan Guard", 30, 'Steel Helmet')
 let empireGuard = new enemies ("Imperial Soldier", 35, "Steel Shield")
-let stormCloak = new enemies ("Stormcloak Soldier", 25, "Light Boots")
+
+// Create a function that extract an item from the enemy
+let stormCloak = ["Stormcloak Soldier", 25, "Light Boots"]
+stormCloak.pop("Light Boots");
+console.log(stormCloak);
+// The create a function that push that item to the player
+hero.push("Light Boots ");
+console.log(hero);
 
 // while (hero.isAlive()) {
 //     const input = readlineSync.keyIn("[w]walk\n[p]print\n[q]quit", {limit: 'wpq'})
@@ -38,114 +47,114 @@ let stormCloak = new enemies ("Stormcloak Soldier", 25, "Light Boots")
 
  /////////// FUNCTIONS \\\\\\\\\\
 
-function diceRoll(min, max) {
-    return Math.floor(Math.random() * (max-min) + min);
-}
-function enemyEncounter(enemies) {
-    diceRoll(1, 4)
-    if (diceRoll === 1) {
-        enemies = helgenGuard
-    } else if (diceRoll === 2) {
-        enemies = empireGuard
-    } else if (diceRoll === 3) {
-        enemies = stormCloak
-    }
-}
-readlineSync.question('Welcome to Helgan keep ' + name + ', This is where you will start your journey to find out who you are.\n Press Enter to begin. ');
-var playerName = hero
-const options =['Walk', 'Exit', 'Print'];
+// function diceRoll(min, max) {
+//     return Math.floor(Math.random() * (max-min) + min);
+// }
+// function enemyEncounter(enemies) {
+//     diceRoll(1, 4)
+//     if (diceRoll === 1) {
+//         enemies = helgenGuard
+//     } else if (diceRoll === 2) {
+//         enemies = empireGuard
+//     } else if (diceRoll === 3) {
+//         enemies = stormCloak
+//     }
+// }
+// readlineSync.question('Welcome to Helgan keep ' + name + ', This is where you will start your journey to find out who you are.\n Press Enter to begin. ');
+// var playerName = hero
+// const options =['Walk', 'Exit', 'Print'];
 
-// function getRandomInt(max) {
-//     return Math.floor(Math.random() * max);
+
+// function openInventory () {
+//     console.log(player.inventory)
 // }
 
-// const random = getRandomInt(100)
-// hero.health -= random
-
-// function pickUp() {
-//     let loots = enemies.loots
+// function calculateEnemyDamage () {
+//     console.log(player.userHealth -= enemyAttack)
 // }
 
-function walk() {
-    const attackPower = Math.floor(Math.random() * 3 +1);
-    const enemyCriticalHit = Math.floor(Math.random() * 5);
-    let playerHealth = 20;
+// function checkPlayerHealth () {
+//     console.log(player.userHealth)
+// }
+
+// function checkEnemyHealth () {
+//     console.log(wildPokemonEncounter.wildPokemonHealth)
+// }
+
+// function attackEnemy () {
+//     console.log(attackPower)
+// }
+// function walk(inventory) {
+//     const attackPower = Math.floor(Math.random() * 3 +10);
+//     const enemyCriticalHit = Math.floor(Math.random() * 5);
+//     let playerHealth = 20;
     
-    const index = readlineSync.keyInSelect(options, 'What will you do Dovahhkin?');
+//     const index = readlineSync.keyInSelect(options, 'What will you do Dovahhkin?');
     
-    if (options[index] == 'Exit') {
-        return userHealth = 0;
-    } else if (options[index] == 'Print') {
-        console.log(hero);
-    } else if (options[index] === 'Walk') {
-        let key = Math.random();
-        if (key <= .3) {
-            console.log('Walking around and taking in the scenery of Skyrim.');
-        } else if (key >= .3) {
-            enemyEncounter(enemies)
-            console.log('***  Combat music kicks in  ***' + enemyEncounter + ' stoped you and draw his sword!\n What would you like to do?');         
-            while(playerHealth > 0 ) {
-                const player = readlineSync.question('Enter "a" to attack, or enter "r" to run! ');
-                switch (player) {
-                    case 'r':
-                        const run = Math.random();
-                        if (run < .5) {
-                            console.log("You can't escape! " + enemies + " attacked you, causing a damage total of: " + enemyCriticalHit + " Hit Points.");
-                        } else {
-                            console.log('You ran away successfully. You live to fight another day!');
-                            break;
-                        }
-                        case 'a':
-                            let enemyHealth = enemies ///Temporary fix
-                            enemyHealth -= attackPower;
-                            console.log('You attacked  ' + enemies + ' with ' + attackPower + ' Hit Points.');
+//     if (options[index] == 'Exit') {
+//         return userHealth = 0;
+//     } else if (options[index] == 'Print') {
+//         console.log(hero);
+//     } else if (options[index] === 'Walk') {
+//         let key = Math.random();
+//         if (key <= .3) {
+//             console.log('Walking around and taking in the scenery of Skyrim.');
+//         } else if (key >= .3) {
+//             enemyEncounter(enemies)
+//             console.log('***  Combat music kicks in  ***' + enemyEncounter + ' stoped you and draw his sword!\n What would you like to do?');         
+//             while(playerHealth > 0 ) {
+//                 const player = readlineSync.question('Enter "a" to attack, or enter "r" to run! ');
+//                 switch (player) {
+//                     case 'r':
+//                         const run = Math.random();
+//                         if (run < .5) {
+//                             console.log("You can't escape! " + enemies + " attacked you, causing a damage total of: " + enemyCriticalHit + " Hit Points.");
+//                         } else {
+//                             console.log('You ran away successfully. You live to fight another day!');
+//                             break;
+//                         }
+//                         case 'a':
+//                             let enemyHealth = 10 ///Temporary fix
+//                             enemyHealth -= attackPower;
+//                             console.log('You attacked  ' + enemies + ' with ' + attackPower + ' Hit Points.');
                             
-                            playerHealth -= enemyCriticalHit;
-                            console.log('Enemy attacked you with ' + enemyCriticalHit + ' Hit Points.');
+//                             playerHealth -= enemyCriticalHit;
+//                             console.log('Enemy attacked you with ' + enemyCriticalHit + ' Hit Points.');
                             
-                            if (enemyHealth <= 0) {
-                                loots = []
-                                let prize = loots;
-                                console.log('Nice work! You defeated ' + enemies + '.\n' + enemies + ' left behind: ' + loots ); // + pickUp
-                                if (loot === "Boots") {
-                                    pickUp(loot);
-                                }
-                            } else if (playerHealth <= 0) {
-                                console.log(enemies + ' has defeated you. You died ☠️! You will respawn at the nearest keep.');
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
-        while(hero.playerHealth > 0) {
-            function playerRestore() {
-                playerActive = true;
-                playerHealth = 40;
-            };
-            playerRestore();
-            walk();
-        }
-        
-        
-        
-        // function openInventory () {
-        //     console.log(player.inventory)
-        // }
-        
-        // function calculateEnemyDamage () {
-        //     console.log(player.userHealth -= enemyAttack)
-        // }
-        
-        // function checkUserHealth () {
-        //     console.log(player.userHealth)
-        // }
-        
-        // function checkEnemyHealth () {
-        //     console.log(wildPokemonEncounter.wildPokemonHealth)
-        // }
-        
-        // function attackEnemy () {
-        //     console.log(attackPower)
-        // }
+//                             if (enemyHealth <= 0) {
+//                                 inventory.push(loot)    
+//                                 console.log('Nice work! You defeated ' + enemies + '.\n' + enemies + ' left behind: ' + loots  ); // + pickUp
+//                                 inventory.push(loot) 
+                                
+//                                 } else if (playerHealth <= 0) {
+//                                     console.log(enemies + ' has defeated you. You died ☠️! You will respawn at the nearest keep.');
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//             // function prize(){
+//             //     const winner = selectWinner()
+//             // }
+//             while(hero.playerHealth > 0) {
+//                 function playerRestore() {
+//                     playerActive = true;
+//                     playerHealth = 40;
+//                 };
+//                 playerRestore();
+//                 walk();
+//             }
+            
+            
+            
+            // function getRandomInt(max) {
+            //     return Math.floor(Math.random() * max);
+            // }
+            
+            // const random = getRandomInt(100)
+            // hero.health -= random
+            
+            // function pickUp() {
+                //     let loots = enemies.loots
+                // }
